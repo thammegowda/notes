1240a02466f528075764ae276b85d3a3b2a6a92b
Thamme Gowda <tgowdan@gmail.com>
2015-09-14 18:09:47 -0700

    Added a routine to find alignment of strings

--- text/edit_distance.py
+++ text/edit_distance.py
@@ -5,8 +5,58 @@ from util.arrays import format_2d_array
 import sys
 
 
+def __find_alignment(matrix, i, j, alignments):
+
+    if i == 0 and j == 0:
+        ## this path is right
+        return True
+
+    possible_moves = []
+
+    min_prev =  matrix[i][j]               # this will be updated with minimum of three moves
+    # TODO: simplify this decision tree
+    if i > 0 and j > 0:     # Diagonal Move is possible
+        possible_moves.append((i-1, j-1, matrix[i-1][j-1]))
+        if matrix[i-1][j-1] <= min_prev:
+            min_prev = matrix[i-1][j-1]
+    if i > 0 :               # Horizontal is possible
+        possible_moves.append((i-1, j, matrix[i-1][j]))
+        if matrix[i-1][j] <= min_prev:
+            min_prev = matrix[i-1][j]
+    if j > 0 :               # Vertical is possible
+        possible_moves.append((i, j-1, matrix[i][j-1]))
+        if matrix[i][j-1] <= min_prev:
+            min_prev = matrix[i][j-1]
+
+    for move in possible_moves:
+        if move[2] <= min_prev and __find_alignment(matrix, move[0], move[1], alignments):
+            alignments.append((move[0], move[1]))
+            return True
+    return False
+
 def align_strings(s1, s2):
-    pass
+
+    matrix = lavenshtein_matrix(s1, s2)
+    print format_2d_array(matrix, '#' + s1, '#' + s2)
+
+    # find the minimum path in the matrix
+    # start from last cell
+
+    i, j = len(s1), len(s2)
+    alignments = []
+    __find_alignment(matrix, i, j, alignments)
+
+    n = len(alignments)
+    res = ''
+    for c in range(n):
+        # Look ahead
+        res += s1[alignments[c][0]] if c == n-1 or c < n - 1 and alignments[c][0] != alignments[c + 1][0] else '*'
+    res += '\n'
+    for c in range(n):
+        res += s2[alignments[c][1]] if c == n-1 or c < n - 1 and alignments[c][1] != alignments[c + 1][1] else '*'
+    print res
+
+
 
 def lavenshtein_matrix(s1, s2):
     '''
@@ -62,5 +112,7 @@ def min_edit_distance(s1, s2):
 if __name__ == '__main__':
     s1 = 'intention'
     s2 = 'execution'
-    distance = min_edit_distance(s1, s2)
-    print distance
+    #FIXME: nxm when n != m
+    #distance = min_edit_distance(s1, s2)
+    #print distance
+    align_strings(s1, s2)
