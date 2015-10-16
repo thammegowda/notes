e40cf3566b7272f6ace49f4125e12c46428b835b
Thamme Gowda <tgowdan@gmail.com>
2015-10-15 21:37:45 -0700

    todos added

---
+++ java/src/main/java/me/gowdru/notes/index/package-info.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * This package has implementation of various indexing algorithms
+ *
+ * //TODO: B-Tree indexing
+ * //TODO: B+ Tree Indexing
+ *
+ *  http://www.wikiwand.com/en/Trie
+ *  
+ *
+ */
+package me.gowdru.notes.index;
