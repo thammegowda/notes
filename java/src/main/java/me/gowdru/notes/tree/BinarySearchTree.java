fe926c02761c5c24997a6ba75dcbe1f4d4a03278
Thamme Gowda <tgowdan@gmail.com>
2015-11-06 12:56:48 -0800

    Add bstree height and node count functions

--- java/src/main/java/me/gowdru/notes/tree/BinarySearchTree.java
+++ java/src/main/java/me/gowdru/notes/tree/BinarySearchTree.java
@@ -1,5 +1,7 @@
 package me.gowdru.notes.tree;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,6 +29,10 @@ public class BinarySearchTree {
         public BSTNode(List<Integer> values, BSTNode parent) {
             this.values = values;
             this.parent = parent;
+        }
+
+        public int getValue(){
+            return getValues().get(0);
         }
 
         public List<Integer> getValues() {
@@ -67,41 +73,13 @@ public class BinarySearchTree {
         public void setRight(BSTNode right) {
             this.right = right;
         }
-
-        /**
-         * Inserts the argument value at right place in BST
-         * @param value the new value to be inserted
-         */
-        public void insert(int value) {
-            if (value == this.values.get(0)) {
-                //belongs to same node
-                this.addValue(value);
-            } else if (value <= this.values.get(0)) {
-                //should go left side
-                if (this.left == null) {
-                    //left sub tree not there yet
-                    this.left = new BSTNode(value, this);
-                } else {
-                    //delegate the task to left sub tree
-                    this.left.insert(value);
-                }
-            } else {
-                //should go to the right side
-                if (this.right == null) {
-                    //right sub tree not there yet
-                    this.right = new BSTNode(value, this);
-                } else {
-                    //delegate the task to right sub tree
-                    this.right.insert(value);
-                }
-            }
-        }
-
-
     }
 
     private BSTNode root;
 
+    public BinarySearchTree(){
+
+    }
     /**
      * Creates a BST containing argument items
      * @param items items to be inserted in BST
@@ -158,18 +136,185 @@ public class BinarySearchTree {
             this.root = new BSTNode(value, null);
         } else {
             //else tell root node to (recursively) find a place for this new value
-            this.root.insert(value);
+            BSTNode node = this.root;
+            while (true){
+                if (value == node.getValues().get(0)) {
+                    //found the place. duplicate value
+                    node.addValue(value);
+                    break;
+                } else if (value < node.getValues().get(0)) {
+                    //no left tree, create one
+                    if (node.getLeft() == null) {
+                        //found the place
+                        node.setLeft(new BSTNode(value, node));
+                        break;
+                    } else {
+                        node = node.getLeft();
+                    }
+                } else {
+                    if (node.getRight() == null) {
+                        //found the place
+                        //no right tree, create one
+                        node.setRight(new BSTNode(value, node));
+                        break;
+                    } else {
+                        node = node.getRight();
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Counts number of nodes in the tree
+     * @return number of nodes in the tree
+     */
+    public long countNodes(){
+        return countChildNodes(this.root);
+    }
+
+    /**
+     * Counts the number of child nodes including given node
+     * @param node the root of sub tree
+     * @return number of nodes, duplicates excluded
+     */
+    private long countChildNodes(BSTNode node) {
+        if (node == null) {
+            return 0;
+        }
+        // left + this + right
+        return countChildNodes(node.getLeft()) + 1
+                + countChildNodes(node.getRight());
+    }
+
+
+    /**
+     * Counts number of values inserted into the tree
+     * @return number of value items inserted
+     */
+    public int size() {
+        return countChildElements(this.root);
+    }
+
+    /**
+     * Counts number of child elements, treats duplicates as separate values
+     * @param node the root of subtree
+     * @return number of values including duplicates
+     */
+    private int countChildElements(BSTNode node){
+        if (node == null) {
+            return 0;
+        }
+        // left + this + right
+        return countChildElements(node.getLeft())
+                +  node.getValues().size()
+                + countChildElements(node.getRight());
+    }
+
+    /**
+     * Finds the total height of tree
+     * @return height of tree
+     */
+    public int findHeight(){
+        //root is zero
+        return findHeight(this.root) - 1;
+    }
+
+    /**
+     * recursively finds height of subtree starting at given node
+     * @param node root of sub tree
+     * @return height of sub tree
+     */
+    private int findHeight(BSTNode node){
+        if (node == null) {
+            return 0;
+        }
+        //this node (1) + longest sub tree (it could be on left side or right side)
+        return 1 + Math.max(findHeight(node.getLeft()), findHeight(node.getRight()));
+    }
+
+    /**
+     * finds the depth of the node having given value
+     * @param value the value whose depth needs to be computed
+     * @return the depth of node containing given value
+     */
+    public int findDepth(int value) {
+        int depth = 0;
+        //start from root
+        BSTNode node = this.root;
+        while (node != null) {
+            if (value == node.getValues().get(0)){
+                //found
+                return depth;
+            }
+            //move down
+            depth++;
+            // decide left or right
+            node = value < node.getValues().get(0) ? node.getLeft() : node.getRight();
+        }
+        // not found
+        return -1;
+    }
+
+
+    /**
+     * tells if the tree is empty or not
+     * @return true if there are no nodes in the ree
+     */
+    public boolean isEmpty(){
+        return this.root == null;
+    }
+
+    /**
+     * Finds the min node in the tree
+     * @return the node with least value
+     */
+    private BSTNode findMinNode() {
+        BSTNode node;
+        //traverse left
+        for (node = this.root; node.getLeft() != null; node = node.getLeft());
+        return node;
+    }
+
+    /**
+     * Finds the max node in the tree
+     * @return the node with highest value
+     */
+    private BSTNode findMaxNode() {
+        BSTNode node;
+        //traverse right
+        for (node = this.root; node.getRight() != null; node = node.getRight());
+        return node;
+    }
+
+    private void delete(int val) {
+        //find node
+        BSTNode node = this.root;
+        while (node != null && node.getValue() != val) {
+            node = val < node.getValue()? node.getLeft() : node.getRight();
+        }
+        if (node != null) {
+            //found the node
+            //FIXME: implement
+            throw new NotImplementedException();
         }
     }
 
     public static void main(String[] args) {
-        int items[] = {5 , 4, 6, 3 , 1, 2, 4, 8, 7, 9};
+        int items[] = {5, 4, 6, 3, 1, 2, 4, 8, 7, 9};
         BinarySearchTree bst = new BinarySearchTree(items);
         System.out.println(bst);
         //NOTE: use debugger to analyse the structure of 'bst' object
 
         System.out.println("In order :");
-        System.out.println(bst.inOrderTraverse());
+        //System.out.println(bst.inOrderTraverse());
+        //System.out.println(bst.countNodes());
+        //System.out.println(bst.countValues());
+        System.out.println(bst.findHeight());
+        for (int item : items) {
+            System.out.println(item + ":" + bst.findDepth(item));
+        }
+
 
     }
 }
--- java/src/test/java/me/gowdru/notes/tree/BinarySearchTreeTest.java
+++ java/src/test/java/me/gowdru/notes/tree/BinarySearchTreeTest.java
@@ -14,14 +14,29 @@ import static org.junit.Assert.*;
 public class BinarySearchTreeTest {
 
     @Test
-    public void testInOrderTraverse() throws Exception {
-        int items[] = {5 , 4, 6, 3 , 1, 2, 4, 8, 7, 9};
+    public void testAll() throws Exception {
+        int items[] = {5, 4, 6, 3, 1, 2, 4, 8, 7, 9};
         BinarySearchTree bst = new BinarySearchTree(items);
 
+        int depths[] = {0, 1, 1, 2, 3, 4, 1, 2, 3, 3};
+        //validate structure
+        for (int i = 0; i < items.length; i++) {
+            assertEquals(depths[i], bst.findDepth(items[i]));
+        }
+        assertEquals(-1 , bst.findDepth(100)); // not found
+
         Arrays.sort(items);
+        //test inorder traversal
         List<Integer> inorderList = bst.inOrderTraverse();
         for (int i = 0; i < items.length; i++) {
             assertEquals(items[i], inorderList.get(i).intValue());
         }
+        assertEquals(9, bst.countNodes());
+        assertEquals(10, bst.size());
+        assertEquals(4, bst.findHeight());
+
+
+
+
     }
 }
