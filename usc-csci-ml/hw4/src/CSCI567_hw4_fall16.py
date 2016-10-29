205b9de46d3b7a06e5de3a53aa2787970404684e
Thamme Gowda <thammegowda@apache.org>
2016-10-29 16:15:25 -0700

    Finalize Machine Learnin HW4 Source code for submission

--- usc-csci-ml/hw4/src/CSCI567_hw4_fall16.py
+++ usc-csci-ml/hw4/src/CSCI567_hw4_fall16.py
@@ -9,10 +9,11 @@
 from hw_utils import *
 from copy import copy
 
-data_path = '../data/MiniBooNE_PID.txt'
+data_path = 'MiniBooNE_PID.txt'
 
 # ## Load Data
 ## (c)
+print("## (c) Loading and Normalizing the dataset")
 X_tr, Y_tr, X_te, Y_te = loaddata(data_path)
 print "Train X, Y :", X_tr.shape, Y_tr.shape
 print "Test  X, Y :", X_te.shape, Y_te.shape
@@ -36,35 +37,42 @@ args = {
     'verbose': False
 }
 
-'''
-# FIXME: BEGIN : remove this
+
+# FIXME: BEGIN : This is for quick testing
 args['batch_size'] = 10
-X_tr, Y_tr = X_tr[0:100], Y_tr[:100]
+X_tr, Y_tr = X_tr[:100], Y_tr[:100]
+X_te, Y_te = X_te[:100], Y_tr[:100]
 print "Train X, Y :", X_tr.shape, Y_tr.shape
 # FIXME: END
-''''
-print("\n\n## (d) Linear Activation ")
-archs = [[d_in, d_out], [d_in, 50, d_out], [d_in, 50, 50, d_out], [d_in, 50, 50, 50, d_out]]
-res = testmodels(X_tr, Y_tr, X_te, Y_te, archs, **args)
 
-archs = [[d_in, 50, d_out], [d_in, 500, d_out], [d_in, 500, 300, d_out],
-         [d_in, 800, 500, 300, d_out], [d_in, 800, 800, 500, 300, d_out]]
-res = testmodels(X_tr, Y_tr, X_te, Y_te, archs, **args)
+print("\n\n## (d) Linear Activation ")
+archs = [[d_in, d_out],
+        [d_in, 50, d_out],
+        [d_in, 50, 50, d_out],
+        [d_in, 50, 50, 50, d_out]]
+testmodels(X_tr, Y_tr, X_te, Y_te, archs, **args)
+
+archs = [[d_in, 50, d_out],
+         [d_in, 500, d_out],
+         [d_in, 500, 300, d_out],
+         [d_in, 800, 500, 300, d_out],
+         [d_in, 800, 800, 500, 300, d_out]]
+testmodels(X_tr, Y_tr, X_te, Y_te, archs, **args)
 
 # (e)
 print("\n\n## (e) Sigmoid Activation")
 args['actfn'] = 'sigmoid'
-_ = testmodels(X_tr, Y_tr, X_te, Y_te, archs, **args)
+testmodels(X_tr, Y_tr, X_te, Y_te, archs, **args)
 
 # (f)
 print("\n\n## (f) ReLu Activation")
 args['actfn'] = 'relu'
 args['sgd_lr'] = 5e-4
-_ = testmodels(X_tr, Y_tr, X_te, Y_te, archs, **args)
+testmodels(X_tr, Y_tr, X_te, Y_te, archs, **args)
 
 # (g)
 print("\n\n## (g) Regularization Coefficients")
-archs = [[d_in, 800, 800, 500, 300, d_out]]
+archs = [[d_in, 800, 500, 300, d_out]]
 args['reg_coeffs'] = [1e-7, 5e-7, 1e-6, 5e-6, 1e-5]
 best, _ = testmodels(X_tr, Y_tr, X_te, Y_te, archs, **args)
 best_lambda_noEstop = best[1]
@@ -92,7 +100,7 @@ print("Best Decay", best_decay)
 print("\n\n## (j) SGD Momentum")
 args['reg_coeffs'] = [0.0]
 args['num_epoch'] = 50
-args['sgd_decays'] = [best_decay] # TODO: get this from the best value of previous step
+args['sgd_decays'] = [best_decay]
 args['sgd_Nesterov'] = True
 args['sgd_moms']= [0.99, 0.98, 0.95, 0.9, 0.85]
 best, _ = testmodels(X_tr, Y_tr, X_te, Y_te, archs, **args)
@@ -115,8 +123,11 @@ testmodels(X_tr, Y_tr, X_te, Y_te, archs, **args)
 
 # (l) Grid Search
 print("\n\n## (j) Grid Search ")
-archs = [[d_in, 50, d_out], [d_in, 500, d_out], [d_in, 500, 300, d_out],
-         [d_in, 800, 500, 300, d_out], [d_in, 800, 800, 500, 300, d_out]]
+archs = [[d_in, 50, d_out],
+        [d_in, 500, d_out],
+        [d_in, 500, 300, d_out],
+        [d_in, 800, 500, 300, d_out],
+        [d_in, 800, 800, 500, 300, d_out]]
 args = {
     'actfn':'relu',
     'last_act':'softmax',
