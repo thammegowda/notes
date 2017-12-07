00598039c79974a941409a6532c1a975f327fc38
Thamme Gowda <tg@isi.edu>
2017-12-07 11:26:52 -0800

    reorganize directories

--- scripts/dcleaner.py
+++ scripts/dcleaner.py
@@ -142,9 +142,9 @@ def parenthesis_inner_drop(f, e):
     :param e:
     :return:
     """
-    matched = re.match(r'(.+)\([a-zA-Z0-9\\. ]+\)(.*)', f)
+    matched = re.match(r'(.+)\([a-zA-Z0-9\\. ]+\)(.*)', e)
     if matched:
-        print(">Splitting %s into %s" % (f, matched.groups()))
+        print(">Remove paranthesis %s into %s" % (f, matched.groups()))
         e = ' '.join(filter(lambda x: x, matched.groups()))
     return [(f, e)]
 
@@ -160,9 +160,40 @@ def remove_puncts(f, e):
     return [(f, e)] if f and e else []
 
 
+def no_to_be(f, e):
+    """
+    Removes  'to' or 'be' or both of those tokens in the beginning of phrases
+    For example:
+       be known -> known
+       to drink -> drink
+    :param f:
+    :param e:
+    :return:
+    """
+    e_ = re.sub('^(to\s)?(be\s)?', '', e)
+    if e != e_:
+        print('> NO_TO_BE: %s -> %s' % (e, e_))
+    return [(f, e_)] if e_ else []
+
+
+def ethiopic_comma_syns(f, e):
+    """
+    Splits foreign phrase by ethiopic comma(፣) and treats the parts as synonyms
+    :param f:
+    :param e:
+    :return:
+    """
+    if '፣' in f:
+        parts = list(filter(lambda x: x, map(lambda x: x.strip(), f.split('፣'))))
+        print(">ETHIOPIC_SYNS:%s -> %s" % (f, parts))
+        return [(p, e) for p in parts]
+    else:
+        return [(f, e)]
+
+
 def remove_ending_punct(f, e):
-    f = re.sub(r"[,\\.]$", '', f)
-    e = re.sub(r"[,\\.]$", '', e)
+    f = re.sub(r"[,\\.]+$", '', f)
+    e = re.sub(r"[,\\.]+$", '', e)
     return [(f, e)]
 
 
@@ -180,11 +211,65 @@ def spellcheck(f, e):
         print(">Spell Correct: %s | %s -> %s" % (f, e, e_))
     return [(f, e_)]
 
+
+class CorefMem(object):
+    def __init__(self):
+        self.index = {}  # Multi map
+        self.pending = {}  # multi map
+
+    def add_entry(self, f, e):
+
+        if f not in self.index:
+            self.index[f] = set()
+        self.index[f].add(e)
+
+    def queue(self, f1, f2):
+        """
+        Add to the Queue, that F2 is same as F1.
+        :param f1:
+        :param f2:
+        :return:
+        """
+        if f1 not in self.pending:
+            self.pending[f1] = set()
+        self.pending[f1].add(f2)
+
+    def get_all(self, tag='coref'):
+        for f1, syns in self.pending.items():
+            if f1 not in self.index:
+                print(">COREF_ERROR: Cant resolve %s; no entry for %s" % (syns, f1))
+                continue
+            for f2 in syns: # all the synonyms of f1
+                for e in self.index[f1]: # multiple meanings
+                    yield (tag, f2, e)
+
+cr_mem = CorefMem()
+
+
+def coref_resolver(f, e, mem=cr_mem):
+    """
+    Resolves co references like:
+      F1 -> same as F2
+      F2 -> E2
+    Since the results are passed as stream, it uses memory to resolve the references
+    :param f:
+    :param e:
+    :return:
+    """
+    if e.startswith('same as '):
+        f_ = e.replace('same as', '').strip()
+        mem.queue(f_, f)
+        return []
+    else:
+        mem.add_entry(f, e)
+        return [(f, e)]
+
+
 # Cleaning Functions END
 
 pre_mappers = [lambda k, v: [(k.strip(), v.strip())],    # white space remover
                small_parenthesis]
-post_mappers = [remove_puncts, remove_ending_punct]
+post_mappers = [remove_puncts, no_to_be, remove_ending_punct]
 rules_cache = {}
 
 
@@ -199,6 +284,9 @@ def get_rules(tag):
 
     rules = []
     rules.extend(pre_mappers)
+    if 'il5' in tag:
+        rules.append(ethiopic_comma_syns)
+
     if 'OCR' in tag:
         rules.append(ocr_gibberish_clean)
     if 'tg-ebookOCR' in tag:
@@ -211,10 +299,14 @@ def get_rules(tag):
         rules.append(spellcheck)
     if 'rpi-ne-il6_eng' == tag:
         rules.append(parenthesis_inner_drop)
+    if 'geezexp' in tag:
+        rules.append(comma_synonyms)
+
     if 'il6' in tag:
         rules.append(abbreviations_expand)
 
     rules.extend(post_mappers)
+    rules.append(coref_resolver)
     rules_cache[tag] = rules
     return rules
 
@@ -248,6 +340,7 @@ def cleanup(src_file):
             tag, fgn_word, eng_word = line.split('\t')
             rules = get_rules(tag)
             yield from transform(tag, fgn_word, eng_word, rules)
+        yield from cr_mem.get_all('coref-resolver')
 
 
 def dump_stream(recs, out=None):
