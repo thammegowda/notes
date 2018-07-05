c172eda195b02cbd275b80b4bf058a8fe7f2d43d
Thamme Gowda <tg@isi.edu>
2018-07-05 14:08:10 -0700

    reformat and use logger

--- util/solr.py
+++ util/solr.py
@@ -1,59 +1,55 @@
-__author__ = 'Thamme Gowda N'
-
-'''
-This module offers solr client.
-
-Created on November 20, 2015
-'''
 import json
 import requests
 import time
+import logging as log
 
-current_milli_time = lambda: int(round(time.time() * 1000))
+log.basicConfig(level=log.INFO)
+debug_mode = log.getLogger().isEnabledFor(level=log.DEBUG)
+
+
+def current_milli_time(): return int(round(time.time() * 1000))
+
 
 class Solr(object):
-    '''
+    """
     Solr client  for querying, posting and committing
-    '''
-    def __init__(self, solr_url,
-                 commit_batch=100):
+    """
+
+    def __init__(self, solr_url):
         self.update_url = solr_url + '/update/json'
         self.query_url = solr_url + '/select'
         self.headers = {"content-type": "application/json"}
+        self.posted_items = 0
 
-    def post_items(self, items, commit=False, softCommit=False):
-        ''' post list of items to Solr;
-         '''
+    def post_items(self, items, commit=False, soft_commit=False):
+        """ post list of items to Solr; """
         url = self.update_url
         # Check either to do soft commit or hard commit
-        if commit == True:
+        if commit:
             url = url + '?commit=true'
-        elif softCommit or 'soft' == commit:
+        elif soft_commit:
             url = url + '?softCommit=true'
 
-        resp = requests.post(
-                    url,
-                    data=json.dumps(items).encode('utf-8', 'replace'),
-                    headers=self.headers)
-
+        resp = requests.post(url,
+            data=json.dumps(items).encode('utf-8', 'replace'),
+            headers=self.headers)
 
         if not resp or resp.status_code != 200:
-            print('Solr posting failed')
+            log.error(f'Solr posting failed {resp.status_code}')
             return False
         return True
 
-    def post_iterator(self, iter, commit=False, softCommit=False, buffer_size=100,
-                      progress_delay=2000):
-        '''
+    def post_iterator(self, iter, commit=False, soft_commit=False, buffer_size=100, progress_delay=2000):
+        """
         Posts all the items yielded by the input iterator to Solr;
         The documents will be buffered and sent in batches
         :param iter: generator that yields documents
         :param commit: commit after each batch? default is false
-        :param softCommit: soft commit after each call ? default is false
+        :param soft_commit: soft commit after each call ? default is false
         :param buffer_size: number of docs to buffer and post at once
         :param progress_delay: the number of milliseconds of
         :return: (numDocs, True) on success, (numDocs, False) on failure
-        '''
+        """
         buffer = []
         count = 0
         num_docs = 0
@@ -63,44 +59,44 @@ class Solr(object):
             buffer.append(doc)
 
             if len(buffer) >= buffer_size:
-                #buffer full, post them
+                # buffer full, post them
                 count += 1
-                if self.post_items(buffer, commit=commit, softCommit=softCommit):
+                if self.post_items(buffer, commit=commit, soft_commit=soft_commit):
                     # going good, clear them all
                     del buffer[:]
                 else:
-                    print('Solr posting failed. batch number=%d' % count)
-                    return (num_docs, False)
+                    log.error(f'Solr posting failed. batch number={count}')
+                    return num_docs, False
 
             if (current_milli_time() - tt) > progress_delay:
                 tt = current_milli_time()
-                print("%d batches, %d docs " % (count, num_docs))
+                log.info("%d batches, %d docs " % (count, num_docs))
 
         res = True
-        if len(buffer) > 0:
-            res = self.post_items(buffer, commit=commit, softCommit=softCommit)
-        return (num_docs, res)
+        if buffer:
+            res = self.post_items(buffer, commit=commit, soft_commit=soft_commit)
+        return num_docs, res
 
     def commit(self):
-        '''
+        """
         Commit index
-        '''
+        """
         resp = requests.post(self.update_url + '?commit=true')
         if resp.status_code == 200:
             self.posted_items = 0
         return resp
 
     def query(self, query='*:*', start=0, rows=20, **kwargs):
-        '''
+        """
         Queries solr and returns results as a dictionary
         returns None on failure, items on success
-        '''
+        """
         payload = {
-                   'q': query,
-                    'wt': 'python',
-                    'start': start,
-                    'rows': rows
-                   }
+            'q': query,
+            'wt': 'python',
+            'start': start,
+            'rows': rows
+        }
 
         if kwargs:
             for key in kwargs:
@@ -110,19 +106,19 @@ class Solr(object):
         if resp.status_code == 200:
             return eval(resp.text)
         else:
-            print(resp.status_code)
+            log.error(f'Status: {resp.status_code}')
             return None
 
     def query_raw(self, query='*:*', start=0, rows=20, **kwargs):
-        '''
+        """
         Queries solr server and returns raw Solr resonse
-        '''
+        """
         payload = {
-                   'q': query,
-                    'wt': 'python',
-                    'start': start,
-                    'rows': rows
-                   }
+            'q': query,
+            'wt': 'python',
+            'start': start,
+            'rows': rows
+        }
 
         if kwargs:
             for key in kwargs:
@@ -131,15 +127,11 @@ class Solr(object):
         return requests.get(self.query_url, params=payload)
 
     def query_iterator(self, query='*:*', start=0, rows=20, **kwargs):
-        '''
+        """
         Queries solr server and returns Solr response  as dictionary
         returns None on failure, iterator of results on success
-        '''
-        payload = {
-                   'q': query,
-                    'wt': 'python',
-                    'rows': rows
-                   }
+        """
+        payload = {'q': query, 'wt': 'python', 'rows': rows}
 
         if kwargs:
             for key in kwargs:
@@ -148,10 +140,10 @@ class Solr(object):
         total = start + 1
         while start < total:
             payload['start'] = start
-            print('start = %s, total= %s' % (start, total))
+            log.debug('start = %s, total= %s' % (start, total))
             resp = requests.get(self.query_url, params=payload)
             if not resp:
-                print('no response from solr server!')
+                log.warning('no response from solr server!')
                 break
 
             if resp.status_code == 200:
@@ -161,21 +153,12 @@ class Solr(object):
                     start += 1
                     yield doc
             else:
-                print(resp)
-                print('Oops! Some thing went wrong while querying solr')
-                print('Solr query params = %s', payload)
+                log.error(resp)
+                log.error('Oops! Some thing went wrong while querying solr')
+                log.error('Solr query params = %s', payload)
                 break
 
     def __del__(self):
-        ''' commit pending docs before close '''
-        print('Solr: commit pending docs before close ...')
-        print('Solr: status = ', self.commit())
-
-
-if __name__ == '__main__':
-    solr = Solr("http://localhost:8983/solr")
-    docs = solr.query_iterator(fl="id")
-    for doc in docs:
-        print(doc)
-    print('Done')
-
+        """ commit pending docs before close """
+        log.info('Solr: commit pending docs before close ...')
+        log.info('Solr: status = %s' % self.commit())
